4d85ccc030319628fff77349446c5c7c07d7d156
A_Z_Kortone <azkortone@gmail.com>
2022-06-28 02:58:27 +0900

    Update Version and Remove unused code

--- TownOfSuper/Main.cs
+++ TownOfSuper/Main.cs
@@ -15,16 +15,13 @@ namespace TownOfSuper
 
         public static ConfigEntry<bool>? debugTool { get; set; }
         public static ConfigEntry<string>? StereotypedText { get; set; }
-
-        public static List<(string, byte)>? MessagesToSend;
-        public static ConfigEntry<int>? MessageWait { get; private set; }
         public Harmony Harmony = new Harmony(Id);
+
         public override void Load()
         {
+
             debugTool = Config.Bind("Client Options", "Debug Tool", false);
             StereotypedText = Config.Bind("Client Options", "StereotypedText", "TownOfSuper定型文");
-            MessagesToSend = new List<(string, byte)>();
-            MessageWait = Config.Bind("Other", "MessageWait", 1);
 
             Harmony.PatchAll();
         }
@@ -44,7 +41,7 @@ namespace TownOfSuper
     {
         public static void Postfix(VersionShower __instance)
         {
-            __instance.text.text += " & <color=#4169e1>TownOfSuper</color> & <color=#200010>AZ</color> ver." + TosPlugin.Version;
+            __instance.text.text += " & <color=#4169e1>TownOfSuper</color> ver." + TosPlugin.Version; //<color=#ffddef>AZ</color> 
         }
     }
 }
